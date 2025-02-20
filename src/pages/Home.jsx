b847899e677ad3b60f0a13fc552216aa9659686d
Mapur2 <rupammodak1@gmail.com>
2025-02-20 18:56:48 +0530

    Somechanges

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,22 @@
-import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
 import MarketStats from "../components/MarketStats";
 import WhyUs from "../components/WhyUs";
-import FeatureSections from "../components/FeatureSections";
 import FAQ from "../components/FAQ";
 import Footer from "../components/Footer";
+import InventoryEdge from '../components/InventoryEdge'
+import ExpenseEdge from '../components/ExpenseEdge'
+import BillEdge from '../components/BillEdge'
 export default function Home() {
-    return (
-      <div>
-        <Navbar />
-        <HeroSection />
-        <MarketStats />
-        <WhyUs />
-        <FeatureSections />
-        <FAQ />
-        <Footer />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <HeroSection />
+      <MarketStats />
+      <WhyUs />
+      <InventoryEdge />
+      <ExpenseEdge />
+      <BillEdge />
+      <FAQ />
+      <Footer />
+    </div>
+  );
+}
