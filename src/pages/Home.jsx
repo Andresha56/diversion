4499fed38ed7f18ca9278b56b9ff885c959c043e
arijitk930 <arijitk930@gmail.com>
2025-02-21 14:55:15 +0530

    done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,19 +2,17 @@ import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
 import MarketStats from "../components/MarketStats";
 import WhyUs from "../components/WhyUs";
-import FeatureSections from "../components/FeatureSections";
 import FAQ from "../components/FAQ";
 import Footer from "../components/Footer";
 export default function Home() {
-    return (
-      <div>
-        <Navbar />
-        <HeroSection />
-        <MarketStats />
-        <WhyUs />
-        <FeatureSections />
-        <FAQ />
-        <Footer />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Navbar />
+      <HeroSection />
+      <MarketStats />
+      <WhyUs />
+      <FAQ />
+      <Footer />
+    </div>
+  );
+}
